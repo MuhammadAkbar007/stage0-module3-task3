5c17f25d02585913054273b565fb0dd4a31f30c8
MuhammadAkbar007 <fotismenos007@mail.ru>
2024-03-19 16:24:02 +0500

    firstAttempt 🥇

--- src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
+++ src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.module3.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+        int first;
+        int second;
+        int third;
+
+        first = 1;
+        third = 100;
+        second = 10;
+
+        System.out.println(first + "\n" + second + "\n" + third);
     }
 }
